3aa93d5f93af3f64a39fecceef523ab4eed0e167
Sheah Wen Liaw <sheahwen@gmail.com>
2025-05-03 10:55:51 +0800

    Add app_name

--- src/app/utils/contants.ts
+++ src/app/utils/contants.ts
@@ -1,2 +1,3 @@
 export const HARDCODED_USER_ID = '00000000-0000-0000-0000-000000000000';
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL = 'http://localhost:3001/api';
+export const APP_NAME = 'DUCK FARM NEXTJS';
